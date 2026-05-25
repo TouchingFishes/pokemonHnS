4015528663a914c00eae371dc458d2c77c387066
TouchingFishes <lutzonino03@gmail.com>
2026-05-25 11:47:43 +0200

    Mlloy now also affected by stick

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1320,6 +1320,7 @@ static void Cmd_critcalc(void)
                 + (holdEffect == HOLD_EFFECT_SCOPE_LENS)
                 + 2 * (holdEffect == HOLD_EFFECT_LUCKY_PUNCH && gBattleMons[gBattlerAttacker].species == SPECIES_CHANSEY)
                 + 2 * (holdEffect == HOLD_EFFECT_STICK && gBattleMons[gBattlerAttacker].species == SPECIES_FARFETCHD);
+                + 2 * (holdEffect == HOLD_EFFECT_STICK && gBattleMons[gBattlerAttacker].species == SPECIES_MLLOY);
 
     if (critChance >= ARRAY_COUNT(sCriticalHitChance))
         critChance = ARRAY_COUNT(sCriticalHitChance) - 1;
