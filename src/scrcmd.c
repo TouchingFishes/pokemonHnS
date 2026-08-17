9987421608aafb3d50488e7734f078fcd14c8475
TouchingFishes <lutzonino03@gmail.com>
2026-08-17 20:06:34 +0200

    Anchored Weekday again....

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -733,8 +733,10 @@ bool8 ScrCmd_initclock(struct ScriptContext *ctx)
 {
     u8 hour = VarGet(ScriptReadHalfword(ctx));
     u8 minute = VarGet(ScriptReadHalfword(ctx));
+    u8 weekDay = GetWeekDay();
 
     RtcInitLocalTimeOffset(hour, minute);
+    SetWeekDay(weekDay);
     return FALSE;
 }
 
